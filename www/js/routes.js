f1dbe8dcad7ea94241fafc5538ff5f3bd13dc0c5
zulmiross <zulejesus@hotmail.com>
2024-04-09 16:00:19 -0300

    update

--- www/js/routes.js
+++ www/js/routes.js
@@ -18,7 +18,7 @@ var app = new Framework7({
     // Add default routes
     routes: [{
             path: '/home/',
-            url: 'index.html',
+            url: 'home.html',
             animate: false,
             on: {
                 pageBeforeIn: function(event, page) {
@@ -29,6 +29,57 @@ var app = new Framework7({
                 },
                 pageInit: function(event, page) {
                     // fazer algo quando a página for inicializada
+                    $.getScript('js/home.js')
+                    var swiper = new Swiper(".sliders", {
+                        slidesPerView: 1,
+                        spaceBetween: 30,
+                        autoplay: {
+                            delay: 2000,
+                        },
+                        loop: true,
+                        breakpoints: {
+                            50: {
+                                slidesPerView: 1,
+                                spaceBetween: 30,
+                            },
+                            640: {
+                                slidesPerView: 2,
+                                spaceBetween: 30,
+                            },
+                            940: {
+                                slidesPerView: 3,
+                                spaceBetween: 30,
+                            },
+                            1200: {
+                                slidesPerView: 4,
+                                spaceBetween: 30,
+                            }
+                        },
+                    })
+                    var swiper2 = new Swiper(".categorias", {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                        loop: false,
+                        breakpoints: {
+                            50: {
+                                slidesPerView: 3,
+                                spaceBetween: 30,
+                            },
+                            640: {
+                                slidesPerView: 6,
+                                spaceBetween: 30,
+                            },
+                            940: {
+                                slidesPerView: 8,
+                                spaceBetween: 30,
+                            },
+                            1200: {
+                                slidesPerView: 10,
+                                spaceBetween: 30,
+                            }
+                        },
+                    })
+
                 },
                 pageBeforeRemove: function(event, page) {
                     // fazer algo antes da página ser removida do DOM
@@ -97,7 +148,7 @@ var app = new Framework7({
 });
 
 //Para testes direto no navegador
-var mainView = app.views.create('.view-main', { url: '/index/' });
+var mainView = app.views.create('.view-main', { url: '/home/' });
 
 //EVENTO PARA SABER O ITEM DO MENU ATUAL
 app.on('routeChange', function(route) {
@@ -115,12 +166,12 @@ app.on('routeChange', function(route) {
 
 function onDeviceReady() {
     //Quando estiver rodando no celular
-    var mainView = app.views.create('.view-main', { url: '/index/' });
+    var mainView = app.views.create('.view-main', { url: '/home/' });
 
     //COMANDO PARA "OUVIR" O BOTAO VOLTAR NATIVO DO ANDROID 	
     document.addEventListener("backbutton", function(e) {
 
-        if (mainView.router.currentRoute.path === '/index/') {
+        if (mainView.router.currentRoute.path === '/home/') {
             e.preventDefault();
             app.dialog.confirm('Deseja sair do aplicativo?', function() {
                 navigator.app.exitApp();
